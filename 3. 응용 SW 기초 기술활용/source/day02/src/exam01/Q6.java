97d1c46382c82bacf39a2608660f41e513151b95
0taelove <0taelove@gmail.com>
2024-04-17 13:57:43 +0900

    gitignore 추가

---
+++ 3. 응용 SW 기초 기술활용/source/day02/src/exam01/Q6.java
@@ -0,0 +1,11 @@
+package exam01;
+
+public class Q6 {
+    public static void main(String[] args) {
+        int[] i = {20, 30, 15, 9};
+
+        for (int j = 0; j < 4 / 2; j++) {
+            System.out.println([j]);
+        }
+    }
+}
