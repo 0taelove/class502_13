ebf41953c7a99c7651c927b1b4de2acf39ee4899
0taelove <0taelove@gmailc.com>
2024-04-24 17:55:17 +0900

    4.24

--- 4. 프로그래밍 언어활용/source/day07/src/exam05/Ex02.java
+++ 4. 프로그래밍 언어활용/source/day07/src/exam05/Ex02.java
@@ -2,7 +2,7 @@ package exam05;
 
 public class Ex02 {
     public static void main(String[] args) {
-        Object[] humans = new Object[2];
+        Object[] humans = new Object[2]; // Human[]은 X Object[]는 최상위 클래스로 가능
         humans[0] = new Human();
         humans[1] = new Tiger();
     }
