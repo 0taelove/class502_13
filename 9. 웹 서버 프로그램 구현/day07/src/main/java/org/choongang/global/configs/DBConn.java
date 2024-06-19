08a8a562ae7ebe3d8f30acf905d92bbf76eef0bd
0taelove <0taelove@gmailc.com>
2024-06-19 11:36:08 +0900

    6.19 컨트롤러 뷰 - 회원가입, 로그인

---
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/global/configs/DBConn.java
@@ -0,0 +1,36 @@
+package org.choongang.global.configs;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+// 쿼리할 수 있는 기능
+public class DBConn {
+    // 객체 만들고 바로 사용 -> static으로 정의
+    private static SqlSessionFactory factory;
+
+    // 객체 만든 후 초기화
+    static {
+        try {
+            Reader reader = Resources.getResourceAsReader("org/choongang/global/configs/mybatis-config.xml");
+
+            factory = new SqlSessionFactoryBuilder().build(reader);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 바로 커밋할 수 있게 설정
+    public static SqlSession getSession(boolean autoCommit) {
+        return factory.openSession(autoCommit);
+    }
+
+    public static SqlSession sqlSession() {
+        return getSession(true);
+    }
+}
---
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/controllers/JoinController.java
@@ -0,0 +1,27 @@
+package org.choongang.member.controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/member/join")
+public class JoinController extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // get 방식일 때는 버퍼를 치환하는 방식으로 작동 forward
+        // 가져오는 객체가 getRequestDispatcher
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/templates/member/join.jsp");
+        rd.forward(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
