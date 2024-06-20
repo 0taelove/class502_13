47ed75a9572b7b9b886fa2ad428a62d3750aafe3
0taelove <0taelove@gmailc.com>
2024-06-20 17:07:37 +0900

    로그인 -> 로그아웃 페이지들 생성

--- 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/controllers/LoginController.java
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/member/controllers/LoginController.java
@@ -6,8 +6,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.choongang.global.exceptions.CommonException;
+import org.choongang.member.services.LoginService;
+import org.choongang.member.services.MemberServiceProvider;
 
 import java.io.IOException;
+
+import static org.choongang.global.MessageUtil.*;
 
 @WebServlet("/member/login")
 public class LoginController extends HttpServlet {
@@ -20,8 +25,17 @@ public class LoginController extends HttpServlet {
         rd.forward(req, resp);
     }
 
+    // 예외 있으면 실패, 예외가 있으면 성공 -> 잘 처리 된 것
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            LoginService service = MemberServiceProvider.getInstance().loginService();
+            service.process(req);
+
+            go(req.getContextPath() + "/", "parent", resp);
+        } catch (CommonException e) {
+            alertError(e, resp);
+        }
 
     }
 }
