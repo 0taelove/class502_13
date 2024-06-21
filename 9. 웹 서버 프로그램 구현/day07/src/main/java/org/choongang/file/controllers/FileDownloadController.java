01d39d9b82bb782e783d63d89466832a9f3c1bad
0taelove <0taelove@gmailc.com>
2024-06-21 14:54:08 +0900

    + 주석 설명 추가

--- 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/file/controllers/FileDownloadController.java
+++ 9. 웹 서버 프로그램 구현/day07/src/main/java/org/choongang/file/controllers/FileDownloadController.java
@@ -19,14 +19,18 @@ public class FileDownloadController extends HttpServlet {
         String fileName = new String(file.getName().getBytes(), "ISO8859_1");
 
         // 다운로드 할 때 중요한 응답 헤더 Content-Disposition
+        // 출력을 화면이 아닌 파일쪽으로 방향을 바꿈
         resp.setHeader("Content-Disposition", "attachment; filename=" + fileName);
         resp.setContentType(contentType);
+        // 네트워크를 거치지 않고 캐시화된 데이터를 사용자에게 제공해줌
         resp.setHeader("Cache-Control", "must-revalidate");
-        // 완료 시간 없애야함
+        // 완료 시간 없애야함 -> 리소스가 이미 만료되었음을 의미
         resp.setIntHeader("Expires", 0);
         // 파일 길이
         resp.setContentLengthLong(file.length());
 
+        // BufferedInputStream : byte단위로 파일을 읽어 올때 사용
+        // etOutputStream : 파일의 데이터를 Http 응답의 바디에 사용 가능 -> 브라우저는 해당 응답을 받아 파일을 다운 받음
         try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
             OutputStream out = resp.getOutputStream();
             out.write(bis.readAllBytes());
